b30365eaf7379df9440a81c2c15b8bc956d65aca
Case Nelson <case@teammating.com>
2012-06-07 08:15:24 -0600

    first commit

---
+++ chars.js
@@ -0,0 +1,22 @@
+TemplateCharacter = {
+    str: 1146,
+    dex: 124,
+    int: 246,
+    vit: 696,
+    armor: 4681,
+    res: 284,
+    life: 16,
+    main_min: 212,
+    main_max: 413,
+    main_aps: 1.2,
+    off_min: 0, //186,
+    off_max: 0, //350,
+    off_aps: 0, //1.2,
+    dmg_bonus_min: 6,
+    dmg_bonus_max: 12,
+    crit_chance: 0.05,
+    crit_bonus: 0.75,
+    aps: 1.2
+}
+
+
